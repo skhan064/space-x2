fb92f44229a112f646c1c3e5effec1c105f1258f
skhan064 <skhan064@fiu.edu>
2020-09-14 22:40:08 -0400

    Add files via upload

---
+++ space-x2/src/main/java/com/wfs/graphql/spacex2/SpaceX2Application.java
@@ -0,0 +1,13 @@
+package com.wfs.graphql.spacex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceX2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceX2Application.class, args);
+	}
+
+}
---
+++ space-x2/src/test/java/com/wfs/graphql/spacex2/SpaceX2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wfs.graphql.spacex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceX2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
